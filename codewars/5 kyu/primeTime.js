8701bb1fa61529b853b715d9cc9b5400a21ac2c0
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-05-10 23:14:49 +0800

    added new kata exercises

--- codewars/5 kyu/primeTime.js
+++ codewars/5 kyu/primeTime.js
@@ -19,8 +19,9 @@ function prime(num) {
 
 
 function isPrime(n) {
-    for(let i = 2, s = Math.sqrt(n); i <= s; i++)
+    for(let i = 2, s = Math.sqrt(n); i <= s; i++) {
         if(n % i === 0) return false; 
+    }
     return n > 1;
 }
 
